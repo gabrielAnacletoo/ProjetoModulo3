72e066362d4dc12cd828a18747a6d9d538dd3791
Gabriel <gabrielanacleto159@live.com>
2023-10-05 19:47:37 -0300

    Projeto Modulo 3

--- src/Routers/index.ts
+++ src/Routers/index.ts
@@ -2,7 +2,6 @@ import { Router } from "express";
 import { userRoutes } from "./User/UserRouters";
 import { JobRouter } from "./Jobs/JobsRouters";
 import { TechnologyRouter } from "./Technology/TechnologyRoutes";
-// import { CityRouter } from "./City/City";
 import { Search } from "./CitySearch/CitySearch";
 import { userAuth } from "./Auth/Auth";
 const routes = Router();
@@ -10,7 +9,6 @@ const routes = Router();
 
 routes.use("/jobs", JobRouter)
 routes.use("/technology", TechnologyRouter)
-// routes.use("/city", CityRouter)
 routes.use("/search", Search)
 routes.use("/auth" , userAuth)
 routes.use("/user", userRoutes)
