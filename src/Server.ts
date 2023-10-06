80c0554837caf65f6d16a8c857803d9dc32b0447
Gabriel <gabrielanacleto159@live.com>
2023-10-05 23:08:31 -0300

    Projeto Modulo 3

--- src/Server.ts
+++ src/Server.ts
@@ -1,5 +1,5 @@
 import {app} from './Index'
 
-const port = process.env.PORT || 3333;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server Running at port ${port}`));
