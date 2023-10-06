5f8f623a58db7a48e1d5741d66e936de2618fcaf
Gabriel <gabrielanacleto159@live.com>
2023-10-06 00:29:53 -0300

    Projeto Modulo 3

--- src/Server.ts
+++ src/Server.ts
@@ -1,8 +1,5 @@
 import {app} from './Index'
-import mongoose from "mongoose"
-import { DatabaseConfig } from './Database/Database'
 
-const port = process.env.PORT || 3333
-DatabaseConfig.initialize()
+const port = process.env.PORT || 3333;
 
 app.listen(port, () => console.log(`Server Running at port ${port}`));
