5a2f5e1047842a76a108ce384e9ffd356fccb637
Gabriel <gabrielanacleto159@live.com>
2023-10-06 00:50:23 -0300

    Projeto Modulo 3

--- src/Server.ts
+++ src/Server.ts
@@ -1,15 +1,5 @@
-import { app } from './Index';
-import { DatabaseConfig } from './Database/Database';
+import {app} from './Index'
 
-const startServer = async () => {
-  try {
-    await DatabaseConfig.initialize();
+const port = process.env.PORT || 3333;
 
-    const port = process.env.PORT || 3333;
-    app.listen(port, () => console.log(`Server Running at port ${port}`));
-  } catch (error) {
-    console.error('Error initializing server:', error);
-  }
-};
-
-startServer();
+app.listen(port, () => console.log(`Server Running at port ${port}`));
