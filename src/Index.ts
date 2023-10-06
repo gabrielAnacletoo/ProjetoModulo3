bb190d258b2670957d48da7de8d550196214f7ea
Gabriel <gabrielanacleto159@live.com>
2023-10-06 00:41:59 -0300

    Projeto Modulo 3

--- src/Index.ts
+++ src/Index.ts
@@ -1,16 +1,20 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import cors from "cors" 
-import { DatabaseConfig } from './Database/Database'
-import { routes } from './Routers'
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { DatabaseConfig } from './Database/Database';
+import { routes } from './Routers';
 
-dotenv.config()
-DatabaseConfig.initialize()
+dotenv.config();
 
+const initializeApp = async () => {
+  await DatabaseConfig.initialize();
 
-const app = express()
-app.use(cors({ origin: '*' })) 
-app.use(express.json())
-app.use(routes)
+  const app = express();
+  app.use(cors({ origin: '*' }));
+  app.use(express.json());
+  app.use(routes);
 
-export { app }
+  return app;
+};
+
+export { initializeApp };
--- src/Server.ts
+++ src/Server.ts
@@ -1,5 +1,10 @@
-import {app} from './Index'
+import { initializeApp } from './Index';
 
-const port = process.env.PORT || 3333;
+const startServer = async () => {
+  const app = await initializeApp();
 
-app.listen(port, () => console.log(`Server Running at port ${port}`));
+  const port = process.env.PORT || 3333;
+  app.listen(port, () => console.log(`Server Running at port ${port}`));
+};
+
+startServer();
