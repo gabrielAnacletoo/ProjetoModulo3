054f530133a33a047066918b63aeb3cb15b02959
Gabriel <gabrielanacleto159@live.com>
2023-10-09 20:07:16 -0300

    Att

--- src/App/Technology/Controller/TechnologyController.ts
+++ src/App/Technology/Controller/TechnologyController.ts
@@ -9,19 +9,21 @@ class TechnologyController {
 
     async CreateFromController(req: Request, res: Response) {
         const { body } = req;
-
+    
         const bodyValidation = await TechnologySchemaValidation.isValid(body);
         if (bodyValidation.error) {
             return res.status(bodyValidation.status).json(bodyValidation.error);
         }
         try {
             const Technology = await this.service.CreateFromService(body);
+            if ('error' in Technology && Technology.error) {
+                return res.status(Technology.status).json(Technology);
+            }
             return res.status(STATUS_CODE.CREATED).json(Technology);
         } catch (error) {
             return res.status(STATUS_CODE.BAD_REQUEST).json({ error: 'Preencha os dados corretamente' });
         }
     }
-
 
     // async FindTopFiveGlobal(req: Request, res: Response) {
     //     try {
